a1c7638fe294e279c6cefad5a32b279a4fffcdc5
Archana <67187815+Archana0903@users.noreply.github.com>
2021-03-23 19:11:24 +0530

    Add files via upload

---
+++ VerifyNumberIsEvenOrOdd.java
@@ -0,0 +1,17 @@
+package GitHubCommit;
+
+public class VerifyNumberIsEvenOrOdd {
+	
+	public static void main (String args[]){
+		
+		int num = 27;
+		
+		if (num % 2 == 0){
+			System.out.println("This is even number");
+		}
+		else{
+			System.out.println("This is a Odd Number");
+		}
+	}
+
+}
